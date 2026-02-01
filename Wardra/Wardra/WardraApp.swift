4a17fbcb8dc393276cc30475f1d617e300bce7a8
Fahdah <fahdah.zms@gmail.com>
2026-02-01 11:23:41 +0300

    Add files via upload

---
+++ Wardra/Wardra/WardraApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardraApp.swift
+//  Wardra
+//
+//  Created by Fahdah Alsamari on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct WardraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wardra/WardraTests/WardraTests.swift
@@ -0,0 +1,17 @@
+//
+//  WardraTests.swift
+//  WardraTests
+//
+//  Created by Fahdah Alsamari on 13/08/1447 AH.
+//
+
+import Testing
+@testable import Wardra
+
+struct WardraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
